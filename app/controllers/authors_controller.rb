4b2a5c1dc2d69bcf95bca666ef72e499e3e506df
RemcoVanDerVeen <remco@10kb.nl>
2025-11-19 21:56:34 +0100

    Authors Index

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,40 +1,32 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: %i[ show edit update destroy ]
 
-  # GET /authors or /authors.json
   def index
-    @authors = Author.joins(:authorables)
+    @authors = Author.joins(:author_authorables)
   end
 
-  # GET /authors/1 or /authors/1.json
   def show
   end
 
-  # GET /authors/new
   def new
     @author = Author.new
   end
 
-  # GET /authors/1/edit
   def edit
   end
 
-  # POST /authors or /authors.json
   def create
     @author = Author.new(author_params)
 
     respond_to do |format|
       if @author.save
         format.html { redirect_to author_url(@author), notice: "Author was successfully created." }
-        format.json { render :show, status: :created, location: @author }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @author.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /authors/1 or /authors/1.json
   def update
     respond_to do |format|
       if @author.update(author_params)
@@ -47,7 +39,6 @@ class AuthorsController < ApplicationController
     end
   end
 
-  # DELETE /authors/1 or /authors/1.json
   def destroy
     @author.destroy
 
@@ -58,13 +49,12 @@ class AuthorsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_author
-      @author = Author.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def author_params
-      params.require(:author).permit(:name, :nickname)
-    end
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  def author_params
+    params.require(:author).permit(:name, :nickname)
+  end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,6 @@
 class Author < ApplicationRecord
   validates :nickname, presence: true
+
+  has_many :author_authorables
+  has_many :authorables, through: :author_authorables
 end
